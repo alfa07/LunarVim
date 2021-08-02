422946dff855c9c8d5db5a7464358de6d154646a
Maxim Sokolov <maximsok@gmail.com>
2021-08-01 19:38:51 -0700

    [colors][lsp] change LspReference colors

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -19,9 +19,9 @@ local function lsp_highlight_document(client)
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=#eee8d5
+      hi LspReferenceText cterm=bold ctermbg=red guibg=#eee8d5
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#eee8d5
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
