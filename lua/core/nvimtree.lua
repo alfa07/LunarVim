92e2c1e5647b0c31c233efc4f951268bfac52abb
Maxim Sokolov <maximsok@gmail.com>
2021-08-01 19:37:21 -0700

    [nvimtree] disable bafferline offset, barbar uses full width

--- lua/core/nvimtree.lua
+++ lua/core/nvimtree.lua
@@ -73,14 +73,14 @@ M.toggle_tree = function()
   end
   if view.win_open() then
     require("nvim-tree").close()
-    if package.loaded["bufferline.state"] then
-      require("bufferline.state").set_offset(0)
-    end
+    -- if package.loaded["bufferline.state"] then
+    --   require("bufferline.state").set_offset(0)
+    -- end
   else
-    if package.loaded["bufferline.state"] and lvim.builtin.nvimtree.side == "left" then
-      -- require'bufferline.state'.set_offset(31, 'File Explorer')
-      require("bufferline.state").set_offset(31, "")
-    end
+    -- if package.loaded["bufferline.state"] and lvim.builtin.nvimtree.side == "left" then
+    --   -- require'bufferline.state'.set_offset(31, 'File Explorer')
+    --   require("bufferline.state").set_offset(31, "")
+    -- end
     require("nvim-tree").toggle()
   end
 end
