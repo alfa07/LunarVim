476aac183d1e6d03561dccd6cbbf3d8d2bbee6e6
Maxim Sokolov <maxim.sokolov@datadoghq.com>
2021-10-18 11:04:53 -0700

    [plugins] remove luasnip and cmp_luasnip

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -31,8 +31,8 @@ return {
       require("core.cmp").setup()
     end,
     requires = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
+      -- "L3MON4D3/LuaSnip",
+      -- "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
